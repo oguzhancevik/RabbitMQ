ec145cfa8452e301352b4994396c94ab3241fc30
Oğuzhan Çevik <cevik.ogsan@gmail.com>
2017-11-26 21:58:03 +0300

    RabbitMQ
    RabbitMQ

--- rabbitmq/src/rabbitmq/Consumer.java
+++ rabbitmq/src/rabbitmq/Consumer.java
@@ -11,61 +11,67 @@ import com.rabbitmq.client.QueueingConsumer;
 import com.rabbitmq.client.QueueingConsumer.Delivery;
 
 public class Consumer {
-	
+
 	static String Secim;
 
 	public static void main(String[] args) throws Exception {
-		
-		ConnectionFactory factory=new ConnectionFactory();
-		
-		factory.setHost("localhost");
-		
-		Connection connection=factory.newConnection();
-		
-		Channel channel=connection.createChannel();
-		
-		QueueingConsumer consumer=new QueueingConsumer(channel);
-		
-		System.out.println("Queue seçin (a/b)");
-		Scanner giris=new Scanner(System.in);
-		Secim=giris.nextLine();
-		
-		if(Secim.equalsIgnoreCase("a")||Secim.equalsIgnoreCase("A")){			
-			channel.basicConsume("queueA", consumer);
-		}
-		
-		if(Secim.equalsIgnoreCase("b")||Secim.equalsIgnoreCase("B")){			
-			channel.basicConsume("queueB", consumer);
-		}
-		
-		
-		
-		boolean removeAllUpTo=true;
-		while (true) {
-			Delivery delivery=consumer.nextDelivery(5000);
-			if (delivery==null) {
-				break;
+
+		try {
+			ConnectionFactory factory = new ConnectionFactory();
+
+			factory.setHost("localhost");
+
+			Connection connection = factory.newConnection();
+
+			Channel channel = connection.createChannel();
+
+			QueueingConsumer consumer = new QueueingConsumer(channel);
+
+			System.out.println("Queue seçin (a/b)");
+			Scanner giris = new Scanner(System.in);
+			Secim = giris.nextLine();
+
+			if (Secim.equalsIgnoreCase("a") || Secim.equalsIgnoreCase("A")) {
+				channel.basicConsume("queueA", consumer);
 			}
 
-			long deliveryTag=delivery.getEnvelope().getDeliveryTag();
-			channel.basicAck(deliveryTag, removeAllUpTo);
-			
-			if (processMessage(delivery))
-			{
-				
+			if (Secim.equalsIgnoreCase("b") || Secim.equalsIgnoreCase("B")) {
+				channel.basicConsume("queueB", consumer);
 			}
-			
+
+			boolean removeAllUpTo = true;
+			while (true) {
+				Delivery delivery = consumer.nextDelivery(5000);
+				if (delivery == null) {
+					break;
+				}
+
+				long deliveryTag = delivery.getEnvelope().getDeliveryTag();
+				channel.basicAck(deliveryTag, removeAllUpTo);
+
+				if (processMessage(delivery)) {
+
+				}
+
+			}
+
+			channel.close();
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		
-		channel.close();
-		connection.close();
 
 	}
-	
+
 	private static boolean processMessage(Delivery delivery) throws UnsupportedEncodingException {
-		String msg=new String (delivery.getBody(),"UTF-8");
-		System.out.println("[X] Yeni Mesaj("+Secim+")"+"\n"+msg);
-		return false;
+		try {
+			String msg = new String(delivery.getBody(), "UTF-8");
+			System.out.println("[X] Yeni Mesaj(" + Secim + ")" + "\n" + msg);
+			return false;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return true;
+		}
 
 	}
 
--- rabbitmq/src/rabbitmq/Producer.java
+++ rabbitmq/src/rabbitmq/Producer.java
@@ -9,44 +9,48 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
-
 public class Producer {
 
-	public static void main(String[] args) throws IOException, TimeoutException{
+	public static void main(String[] args) throws IOException, TimeoutException {
 
-		ConnectionFactory factory=new ConnectionFactory();
-		factory.setHost("localhost");
-		
-		Connection connection= factory.newConnection();
-		
-		Channel channel=connection.createChannel();
-		
-		String queueA="queueA";
-		String queueB="queueB";
-		String Secim;
-		String Mesaj;
-		Date Zaman= new Date();
-		
-		System.out.println("Queue secin (a/b)");
-		Scanner giris=new Scanner(System.in);
-		Secim=giris.nextLine();
-		
-		System.out.println("Mesaji giriniz:("+Secim+")");
-		Mesaj=giris.nextLine();
-		System.out.println(Mesaj);
-		
-		Mesaj=Zaman.toString()+" | "+Mesaj+" |";
-		
-		if(Secim.equalsIgnoreCase("a")||Secim.equalsIgnoreCase("A")){			
-			channel.basicPublish("", queueA, null, Mesaj.getBytes());
+		try {
+			ConnectionFactory factory = new ConnectionFactory();
+			factory.setHost("localhost");
+
+			Connection connection = factory.newConnection();
+
+			Channel channel = connection.createChannel();
+
+			String queueA = "queueA";
+			String queueB = "queueB";
+			String Secim;
+			String Mesaj;
+			Date Zaman = new Date();
+
+			System.out.println("Queue secin (a/b)");
+			Scanner Giris = new Scanner(System.in);
+			Secim = Giris.nextLine();
+
+			System.out.println("Mesaji giriniz:(" + Secim + ")");
+			Mesaj = Giris.nextLine();
+			System.out.println(Mesaj);
+
+			Mesaj = Zaman.toString() + " | " + Mesaj + " |";
+
+			if (Secim.equalsIgnoreCase("a") || Secim.equalsIgnoreCase("A")) {
+				channel.basicPublish("", queueA, null, Mesaj.getBytes());
+			}
+
+			if (Secim.equalsIgnoreCase("b") || Secim.equalsIgnoreCase("B")) {
+				channel.basicPublish("", queueB, null, Mesaj.getBytes());
+			}
+
+			channel.close();
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		
-		if(Secim.equalsIgnoreCase("b")||Secim.equalsIgnoreCase("B")){			
-			channel.basicPublish("", queueB, null, Mesaj.getBytes());
-		}
-		
-		channel.close();
-		connection.close();
+
 	}
 
 }
